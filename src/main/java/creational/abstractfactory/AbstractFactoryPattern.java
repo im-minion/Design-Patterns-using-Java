adce88e5c8b50311a1435eb9ed39bd5eb2d00c7c
im-minion <vaibhavminiyar@gmail.com>
2021-01-09 11:00:47 +0530

    added Abstract Factory pattern done

--- src/main/java/creational/abstractfactory/AbstractFactoryPattern.java
+++ src/main/java/creational/abstractfactory/AbstractFactoryPattern.java
@@ -1,5 +1,13 @@
+/*
+ * Creational design patterns provide solution to instantiate a object in the best possible way for specific situations.
+ */
 package creational.abstractfactory;
 
+/*
+* Abstract Factory pattern is similar to Factory pattern and it’s a factory of factories.
+* In Abstract Factory pattern, we get rid of if-else block and have a factory class for each sub-class
+* and then an Abstract Factory class that will return the sub-class based on the input factory class.
+* */
 import creational.factory.Machine;
 
 public class AbstractFactoryPattern {
